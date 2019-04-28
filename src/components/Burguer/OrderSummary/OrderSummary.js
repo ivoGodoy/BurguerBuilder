77112e0fc82bc4b3d05094ccb6d613493deb2c19
Ivo <ivoeg@hotmail.com>
2019-04-28 20:05:37 -0300

    Add shouldComponentUpdate on OrderSummary for better performance

--- src/components/Burguer/OrderSummary/OrderSummary.js
+++ src/components/Burguer/OrderSummary/OrderSummary.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../../hoc/Auxiliary'
 import Button from '../../UI/Button/Button'
+import { isMaster } from 'cluster';
 
-const orderSummary = (props) =>{
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //this could be a functional component it doesnt have to be a class based component.
+    // componentDidUpdate(){
+    //     console.log('[orderSummary] DidUpdate');
+    // };
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map( (igKey) => (
-            <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+            <li key={igKey}>
+            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+            </li>
         ))
-    return(
-        <Aux>
+        return(
+            <Aux>
             <h3>Your order</h3>
             <p>A delicious burguer with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue to checkout?</p>
 
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
